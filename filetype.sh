3ce68779199a6e64aadc0f9097108cf019d6229f
SeokWon Hong <idle99970@naver.com>
2021-05-26 20:09:00 +0900

    test

--- filetype.sh
+++ filetype.sh
@@ -1,2 +1,9 @@
-#!/bin.sh
-
+#!/bin/sh
+read -p "Enter File name or Dir name: " name
+fType=`file -b name`
+if [ "fType" == "" ]; then
+	echo "name" is not exist in Directory.
+	exit
+else
+	echo This file is `fType`
+fi
